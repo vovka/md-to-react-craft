5f7c4d1d955b64291f90fe754a4683d2733b0f61
Volodymyr Shcherbyna <scherbina.v@gmail.com>
2026-07-21 18:08:32 +0300

    Add production comments configuration

---
+++ src/config/comments.ts
@@ -0,0 +1,9 @@
+import type { GiscusConfiguration } from "@/components/comments/GiscusComments";
+
+export const commentsConfig: GiscusConfiguration = {
+  repo: "vovka/blog-comments",
+  repoId: "",
+  category: "Article comments",
+  categoryId: "",
+  canonicalBaseUrl: "https://blog.shcherbyna.me",
+};
