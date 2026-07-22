9b9b2aa7e35f17e446816f5ee7828d17b06f0707
Volodymyr Shcherbyna <scherbina.v@gmail.com>
2026-07-23 01:59:36 +0300

    config real comments repo

--- src/config/comments.ts
+++ src/config/comments.ts
@@ -2,8 +2,8 @@ import type { GiscusConfiguration } from "@/components/comments/GiscusComments";
 
 export const commentsConfig: GiscusConfiguration = {
   repo: "vovka/blog-comments",
-  repoId: "",
-  category: "Article comments",
-  categoryId: "",
+  repoId: "R_kgDOTgmoUg",
+  category: "Announcements",
+  categoryId: "DIC_kwDOTgmoUs4DBwZb",
   canonicalBaseUrl: "https://blog.shcherbyna.me",
 };
