abc06f5309e487247437add777cadecae9649e9f
Volodymyr Shcherbyna <scherbina.v@gmail.com>
2026-07-23 00:05:06 +0300

    Wire analytics provider and privacy route

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route, Navigate, useNavigate, useLocation } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useNavigate, useLocation } from "react-router-dom";
+import AnalyticsProvider from "@/components/analytics/AnalyticsProvider";
 import Blog from "./pages/Blog";
 import BlogPost from "./pages/BlogPost";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import Privacy from "./pages/Privacy";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -34,12 +36,13 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <GitHubPagesRedirect />
+        <AnalyticsProvider />
         <Routes>
           <Route path="/" element={<Blog />} />
-          <Route path="/:slug" element={<BlogPost />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="/privacy" element={<Privacy />} />
+          <Route path="/:slug" element={<BlogPost />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
